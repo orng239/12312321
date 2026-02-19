eedb2df0e6e29dba7c9da280dc5ed44cc8e9531e
orng239 <bebeshka533@gmail.com>
2026-02-19 12:37:03 +0300

    Add files via upload

--- praktika2/src/main/java/org/example/Task.java
+++ praktika2/src/main/java/org/example/Task.java
@@ -1,20 +1,38 @@
 package org.example;
+
 public class Task {
     private int id;
     private String title;
     private String description;
 
     public Task(int id, String title, String description) {
-
-
         this.id = id;
         this.title = title;
         this.description = description;
     }
-    public int getId() {return id;}
-    public void setId(int id) {this.id=id;}
-    public String getTitle() {return title;}
-    public void setTitle(String title) {this.title=title;}
-    public String getDescription() {return description;}
-    public void setDescription(String description) {this.description=description;}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String toString() {
+        return "ид: " + id + ", названи: " + title + ", описание: " + description;
+    }
 }
